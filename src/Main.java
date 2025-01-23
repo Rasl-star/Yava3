ea1e410f976deef6cbcc8747ccc139cff7f14964
Rasl test <tlevaliev94@gmail.com>
2025-01-23 19:20:04 +0400

    change

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 12600;
+        int equivalent = 20;
+        int bonus = amount / equivalent;
+        System.out.println("Итоговый бонус: " + bonus);
+    }
+}
